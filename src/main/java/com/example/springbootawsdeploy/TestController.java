a4176ea66abfd040266679203a07f64886d3cb06
Dhruv Darji <dhruvdarji145@gmail.com>
2023-12-20 12:08:58 +0530

    ECS Name DOCKER Added

--- src/main/java/com/example/springbootawsdeploy/TestController.java
+++ src/main/java/com/example/springbootawsdeploy/TestController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/data")
     public String getData(){
-        return "First message from AWS ECS";
+        return "First message from AWS ECS at DOCKER";
     }    
 }
